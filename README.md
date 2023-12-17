27e58f9ec281badf5b34a42084f96ce213ee8e3f
cnk113 <changkim@Changs-MBP-2.ucsf.edu>
2023-12-17 00:25:39 -0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # xenium_barcode_generation
+
+Current library diversity
+```
+                  Description  hamming   seqlev levenshtein
+1               Mean Distance 34.72835 24.67569    27.60878
+2             Median Distance 35.00000 25.00000    28.00000
+3            Minimum Distance 17.00000 15.00000    15.00000
+4            Maximum Distance 44.00000 32.00000    37.00000
+5 Guaranteed Error Correction  8.00000  7.00000     7.00000
+6  Guaranteed Error Detection 16.00000 14.00000    14.00000
+```
--- barcode_maker.py
+++ barcode_maker.py
@@ -3,7 +3,7 @@ import pandas as pd
 from Levenshtein import hamming
 import sys
 import pyfastx
-
+from Bio.SeqUtils import MeltingTemp as mt
 # python barcode_maker.py NUM_SEQ DISTANCE ITERATIONS > out.txt
 #blacklist = set(['CG', 'GT', 'GG', 'GC'])
 #pref = set(["AT", "TA", "GA", "AG"])
@@ -12,15 +12,15 @@ h = 'Chromium_Human_Transcriptome_Probe_Set_v1.0.1_GRCh38-2020-A.csv'
 m = 'Chromium_Mouse_Transcriptome_Probe_Set_v1.0.1_mm10-2020-A.csv'
 human = pd.read_csv(h, comment="#")
 mouse = pd.read_csv(m, comment="#")
-human = human['probe_seq'].str[5:-5].tolist()
-mouse = mouse['probe_seq'].str[5:-5].tolist()
+human = human['probe_seq'].str[3:-3].tolist()
+mouse = mouse['probe_seq'].str[3:-3].tolist()
 
 hv = 'Visium_Human_Transcriptome_Probe_Set_v2.0_GRCh38-2020-A.csv'
 mv = 'Visium_Mouse_Transcriptome_Probe_Set_v1.0_mm10-2020-A.csv'
 human_visium = pd.read_csv(hv, comment="#")
 mouse_visium = pd.read_csv(mv, comment="#")
-human_visium = human_visium['probe_seq'].str[5:-5].tolist()
-mouse_visium = mouse_visium['probe_seq'].str[5:-5].tolist()
+human_visium = human_visium['probe_seq'].str[3:-3].tolist()
+mouse_visium = mouse_visium['probe_seq'].str[3:-3].tolist()
 
 xenium = []
 for name, seq in pyfastx.Fasta('xenium_human_brain_gene_expression_panel_v1_probe_sequences.fasta', build_index=False, uppercase=True):
@@ -30,34 +30,36 @@ for name, seq in pyfastx.Fasta('xenium_mouse_brain_gene_expression_panel_v1.1_pr
 
 merged = human + mouse + human_visium + mouse_visium + xenium
 merged = set(merged)
-left = [w[:20] for w in merged]
-right = [w[:-20] for w in merged]
+left = [w[:22] for w in merged]
+right = [w[:-22] for w in merged]
 
 with open("re.txt") as file:
     res = [line.rstrip() for line in file]
 library = set()
 for it in range(int(sys.argv[3])):
-    library.update(design.max_orthogonality(int(sys.argv[1]), 40, alphabet="ACGT", RCfree=True, GClims=(16, 29), prevented_patterns=res))
-lib = []
+    library.update(design.max_orthogonality(int(sys.argv[1]), 44, alphabet="ACGT", RCfree=True, GClims=(18, 36), prevented_patterns=res))
 dist = int(sys.argv[2])
 for i in library:
     flag = False
-    lig = i[19:21]
+    melt = mt.Tm_NN(i)
+    if melt < 68 or melt > 82:
+        continue
+    lig = i[20:22]
     if lig == "AT" or lig == "TA" or lig == "GA" or lig == "AG":
         for probe in left:
-            l = i[:20]
+            l = i[:22]
             ham = hamming(l, probe)
-            gc = l.count("G") + l.count("C")
-            if ham > dist and gc > 7 and gc < 16:
+            melt = mt.Tm_NN(l)
+            if ham < dist or melt < 50 or melt > 70:
                 flag = True
                 break
         if flag:
             continue
         for probe in right:
-            r = i[:-20]
+            r = i[:-22]
             ham = hamming(r, probe)
-            gc = r.count("G") + r.count("C")
-            if ham > dist and gc > 7 and gc < 16:
+            melt = mt.Tm_NN(r)
+            if ham < dist or melt < 50 or melt > 70:
                  flag = True
                  break
         if flag:
